03aa3a9b1efd12cf8d44665befbf812dd5ff11ee
João Barros <joaohlbarros@gmail.com>
2022-01-12 01:13:06 +0000

    Add App title

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import PutMessage from "../components/PutMessage";
 import {Fragment} from "react";
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <Fragment>
+        <Head>
+            <title>Next-PutMessage</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
       <Navbar />
       <main className="container">
         <div className="my-3 p-3 bg-body rounded shadow-sm">
